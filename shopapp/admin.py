20af3bb9e70e824e268e8431c85ec11c1d219736
Siarhei <svd@sakema.net>
2019-04-02 16:26:03 +0530

    add, create migration and register model for Brands

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from shopapp.models import Category
+from shopapp.models import Brand
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Brand)
--- shopapp/models.py
+++ shopapp/models.py
@@ -3,5 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-     name = models.CharField(max_length=200)
-     slug = models.SlugField(max_length=200)
+     name = models.CharField(max_length=200, db_index=True)
+     slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+     def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+     name = models.CharField(max_length=200, db_index=True)
+
+     def __str__(self):
+        return self.name
