31d7d187e0594bff8f24f2acbb38b4ac3f6f2e2c
Siarhei <svd@sakema.net>
2019-04-02 15:21:24 +0530

    add model category and mifration for it

--- shopapp/models.py
+++ shopapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+     name = models.CharField(max_length=200)
+     slug = models.SlugField(max_length=200)
