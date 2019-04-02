480edd1aa2f8da5f6a4f1c8b3dc6c318342fa798
Siarhei <svd@sakema.net>
2019-04-02 18:15:42 +0530

    add model for product

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from shopapp.models import Category
-from shopapp.models import Brand
+from shopapp.models import Category, Brand, Product
+
+
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Brand)
+admin.site.register(Product)
--- shopapp/models.py
+++ shopapp/models.py
@@ -3,14 +3,27 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-     name = models.CharField(max_length=200, db_index=True)
-     slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
-     def __str__(self):
+    def __str__(self):
         return self.name
 
 class Brand(models.Model):
-     name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(max_length=200, db_index=True)
 
-     def __str__(self):
+    def __str__(self):
         return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, models.SET_NULL, blank=True, null=True)
+    brand = models.ForeignKey(Brand, models.SET_NULL, blank=True, null=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, db_index=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
