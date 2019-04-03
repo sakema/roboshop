4935125fef93502834f11c19e0474677efe98424
Siarhei <svd@sakema.net>
2019-04-03 11:16:48 +0530

    Include urls from shopapp

--- roboshop/urls.py
+++ roboshop/urls.py
@@ -14,19 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 #for static files in development mode
 from django.conf import settings
 from django.conf.urls.static import static
 
-#application
-from shopapp.views import main
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main, name='main'),
+    path('', include('shopapp.urls')),
 ]
 
 #for development mode
--- shopapp/views.py
+++ shopapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def main(request):
-    return render(request, 'main.html', {})
+def base_view(request):
+    return render(request, 'base.html', {})
