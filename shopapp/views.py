89348e6b6700701ed5c45f5c91ceaa61e3a683db
Siarhei <svd@sakema.net>
2019-04-03 12:12:32 +0530

    create base view

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from shopapp.models import Category, Product
 
 # Create your views here.
 
 def base_view(request):
-    return render(request, 'base.html', {})
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    contex = {
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'base.html', contex)
