e6e24ad2de8f2997719e5605696970b651d30681
Siarhei <svd@sakema.net>
2019-04-03 10:00:25 +0530

    make editable fields in admin panel for product and auto fill slug field for category

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from shopapp.models import Category, Brand, Product
 
+# model for Category in admin panel
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
 
-
+# model for Product in admin panel
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Brand)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
